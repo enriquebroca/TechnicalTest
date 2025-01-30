84ebf78b16463ee02288ea11bd334824f4fbed4d
Luis Vidal <lvidal@barri.com>
2025-01-30 13:00:19 -0600

    unit test

---
+++ ChipSecuritySystem.Test/ChipSecurityTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChipSecuritySystem.Test
+{
+    [TestClass]
+    public class ChipSecurityTest
+    {
+        [TestMethod]
+        public void TestRandomColorChip()
+        {
+            var userChipsEnteredList = new List<ColorChip>();
+            userChipsEnteredList.Add(new ColorChip(Color.Blue, businessLogic.GetRandomColor()));
+            userChipsEnteredList.Add(new ColorChip(businessLogic.GetRandomColor(), businessLogic.GetRandomColor()));
+            userChipsEnteredList.Add(new ColorChip(businessLogic.GetRandomColor(), businessLogic.GetRandomColor()));
+            userChipsEnteredList.Add(new ColorChip(businessLogic.GetRandomColor(), Color.Green));
+            bool chinpsVerify = businessLogic.VerifyChannel(userChipsEnteredList);
+            Assert.IsTrue(chinpsVerify);
+
+        }
+    }
+}
---
+++ ChipSecuritySystem.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChipSecuritySystem.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChipSecuritySystem.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41c110db-c885-48ac-a409-61d5ff7bddda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
